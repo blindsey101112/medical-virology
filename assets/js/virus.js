9d985e4e9974e2c6f9e94fa21916025844b55022
blindsey101112 <105917126+blindsey101112@users.noreply.github.com>
2024-12-31 22:34:24 +0000

    Update virus.js

--- assets/js/virus.js
+++ assets/js/virus.js
@@ -1,1 +1,15 @@
+        function toggleSection(sectionId) {
+            const section = document.getElementById(sectionId);
+            section.style.display = section.style.display === "none" || section.style.display === "" ? "block" : "none";
+        }
 
+        function toggleDetail(button) {
+            const detailedContent = button.previousElementSibling;
+            if (detailedContent.style.display === "none" || detailedContent.style.display === "") {
+                detailedContent.style.display = "block";
+                button.textContent = "Show Less";
+            } else {
+                detailedContent.style.display = "none";
+                button.textContent = "Show More";
+            }
+        }
